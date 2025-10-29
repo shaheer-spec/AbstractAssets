929585499ffd5e8dbd3d8b403813e438f66707b7
Shaheer kayani <shaheer.kayani8@gmail.com>
2025-10-29 11:00:27 -0400

    Initial Commit with code

--- src/main/java/com/pluralsight/Cash.java
+++ src/main/java/com/pluralsight/Cash.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
-public class Cash {
+public class Cash extends Asset{
+    private double money;
+
+    public Cash(String description, String dateAcquired, double originalCost, double money) {
+        super(description, dateAcquired, originalCost);
+        this.money = money;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    @Override
+    public double getValue(){
+        return 0;
+    }
 }
